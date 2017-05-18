d24f8614200e5189b0f94fbe6333a91e8abbc5d2
zuozhiw <zuozhiw@uci.edu>
2017-05-18 15:41:00 -0700

    ignore plan table in backend

--- textdb/textdb-web/src/main/java/edu/uci/ics/textdb/web/resource/SystemResource.java
+++ textdb/textdb-web/src/main/java/edu/uci/ics/textdb/web/resource/SystemResource.java
@@ -1,6 +1,8 @@
 package edu.uci.ics.textdb.web.resource;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+
+import edu.uci.ics.textdb.exp.planstore.PlanStoreConstants;
 import edu.uci.ics.textdb.storage.RelationManager;
 import edu.uci.ics.textdb.storage.TableMetadata;
 import edu.uci.ics.textdb.web.response.TextdbWebResponse;
@@ -8,6 +10,7 @@ import edu.uci.ics.textdb.web.response.TextdbWebResponse;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Path("/resources")
 @Produces(MediaType.APPLICATION_JSON)
@@ -16,6 +19,9 @@ public class SystemResource {
 	@Path("/metadata")
 	public TextdbWebResponse getMetadata() throws Exception {
 		List<TableMetadata> tableMetadata = RelationManager.getRelationManager().getMetaData();
+		tableMetadata = tableMetadata.stream()
+		        .filter(metadata -> metadata.getTableName().equalsIgnoreCase(PlanStoreConstants.TABLE_NAME))
+		        .collect(Collectors.toList());
 		return new TextdbWebResponse(0, new ObjectMapper().writeValueAsString(tableMetadata));
 	}
 }
