fa8270e19beacd19d7b2fbe8201165d59fda286e
BobbyFine <sirencbl@gmail.com>
2018-05-13 22:24:33 -0700

    add dynamic tour steps

--- core/new-gui-dev/src/app/workspace/component/product-tour/product-tour.component.ts
+++ core/new-gui-dev/src/app/workspace/component/product-tour/product-tour.component.ts
@@ -7,29 +7,54 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductTourComponent implements OnInit {
 
-  constructor() { }
+  steps = [
+    {
+      intro: '<h1>Welcome to Texera!</h1><br><p style="font-size:15px;">We will go through the whole process of a workflow</p>',
+    },
+    {
+      element: '.texera-operator-view-grid-container',
+      intro:'This is the operators that you can use. Now open the first section named <b>Source</b>',
+      position: 'right'
+    },
+    {
+      element: '#mat-expansion-panel-header-0',
+      intro:'Now open this section',
+      position:'right'
+    },
+    {
+      element: '#texera-operator-label-ScanSource',
+      intro:'Drag this and drop to workflow',
+      position:'right'
+    }
+  ];
+  intro = require('../../../../../node_modules/intro.js/intro.js').introJs();
+
+  inTour = false;
+
+
+  constructor() {}
 
   ngOnInit() {
   }
 
   startTour() {
-    console.log("Starting tour");
-    const IntroJs = require('../../../../../node_modules/intro.js/intro.js');
-    let intro = IntroJs.introJs();
-
-    // Initialize steps
-    intro.setOptions({
-        steps: [
-            {
-                element: '.texera-navigation-grid-container',
-                intro: "Welcome!",
-                position: 'bottom'
-            }
-        ]
-    });
-
     // Start tutorial
-    intro.start();
+    this.intro.setOptions({
+      steps: this.steps,
+      showStepNumbers: true,
+    });
+    this.intro.start();
+    this.inTour = true;
+    this.intro.onexit(function() {
+      this.inTour = false;
+    });
+    console.log("Test");
+  }
+
+  step(num) {
+    if(this.inTour == true) {
+      this.intro.goToStep(num);
+    };
   }
 
 }
